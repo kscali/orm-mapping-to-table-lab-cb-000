50da568b1052ee41aba16b73cd83740a3e3dbd39
Koy Saeteurn <kosatun@aol.com>
2018-09-14 05:26:09 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,7 +32,7 @@ class Student
     SQL
     DB[:conn].execute(sql, self.name, self.grade)
     
-    @id = DB[:conn].execute("SELECT * FROM students")
+    @id = DB[:conn].execute("SELECT * FROM students")[0]
   end   
   
   def self.create(name:, grade:) 
