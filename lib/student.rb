c86c518950d9e63ca9d8642907868893e98e1539
Koy Saeteurn <kosatun@aol.com>
2018-09-14 05:14:52 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -21,9 +21,13 @@ class Student
   
   def self.drop_table
     sql =  <<-SQL
-      DROP TABLE
+      DROP TABLE students
     SQL
     DB[:conn].execute(sql)
   end
+  
+  def self.save
+    
+  end   
   
 end
